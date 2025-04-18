3c79a408c49876a67756363e1e91aac23c548f07
CC11001100 <CC11001100@qq.com>
2025-04-18 16:14:10 +0800

    feat: 添加GitHub图标和返回链接

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
-import AlgorithmAnimation from './components/algorithm/AlgorithmAnimation'
+import './App.css';
+import AlgorithmAnimation from './components/algorithm/AlgorithmAnimation';
+import GitHubIcon from './assets/github.svg';
 
 function App() {
   return (
     <div className="app-container">
+      <a href="https://fuck-algorithm.github.io/leetcode-hot-100/" target="_blank" rel="noopener noreferrer" className="back-link">
+        ← 返回LeetCode Hot 100
+      </a>
+      <a href="https://github.com/fuck-algorithm/leetcode-11-container-with-most-water" target="_blank" rel="noopener noreferrer">
+        <img src={GitHubIcon} alt="GitHub" className="github-icon" />
+      </a>
       <AlgorithmAnimation />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
